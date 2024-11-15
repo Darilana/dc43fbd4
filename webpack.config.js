e6f098aa22004c305f6b14ff3c95adf3b7702273
Darilana Kudinova <darilana.kudinova@gmail.com>
2024-11-15 10:44:53 -0800

    Created production build

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const HtmlWebPackPlugin = require('html-webpack-plugin');
+const path = require('path');
 
 module.exports = {
     entry: './src/index.ts',
@@ -44,5 +45,6 @@ module.exports = {
     },
     output: {
         publicPath: '/',
+        path: path.resolve(__dirname, 'docs'),
     },
 };
